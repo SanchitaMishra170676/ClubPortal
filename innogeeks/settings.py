647e61dd74c6c391eac0bb06f62a127ac956bc8e
Ubuntu <ubuntu@ip-172-31-5-233.ap-south-1.compute.internal>
2021-05-15 08:25:31 +0000

    added settings

--- innogeeks/settings.py
+++ innogeeks/settings.py
@@ -30,8 +30,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
+#SECURE_SSL_REDIRECT = True
+#SESSION_COOKIE_SECURE = True
 
 # Application definition
 
