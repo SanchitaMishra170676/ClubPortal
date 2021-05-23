53457f2f7f9413f01cb7fa711ad3cc7c8b2f9bbb
Vanshika Singhal <vanshikasinghal69@gmail.com>
2021-05-23 17:04:49 +0530

    minor changes

--- innogeeks/settings.py
+++ innogeeks/settings.py
@@ -89,22 +89,22 @@ WSGI_APPLICATION = 'innogeeks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
-
-
 DATABASES = {
-     'default':{
-	'ENGINE': 'django.db.backends.mysql',
-	'OPTIONS': {
-	    'read_default_file': '/etc/mysql/my.cnf',
-	},
-     }
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+   }
 }
+
+
+# DATABASES = {
+#      'default':{
+# 	'ENGINE': 'django.db.backends.mysql',
+# 	'OPTIONS': {
+# 	    'read_default_file': '/etc/mysql/my.cnf',
+# 	},
+#      }
+# }
 
 
 # DATABASES = {
